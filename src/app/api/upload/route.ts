474f3800118f14301537bb95c976c695bf433d74
Mohisvst29 <mohmad.shiple@gmail.com>
2026-05-03 00:09:49 +0300

    Configure cloudinary explicitly

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,6 +1,12 @@
 import { NextResponse } from "next/server";
 import { v2 as cloudinary } from "cloudinary";
 
+cloudinary.config({
+  cloud_name: "dakojcm6f",
+  api_key: "952829294835597",
+  api_secret: "zdLHZ2oLFKS4DJMm9KvuuU0Xjd4",
+});
+
 export async function POST(req: Request) {
   try {
     const formData = await req.formData();
